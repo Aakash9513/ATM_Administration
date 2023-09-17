e608ead05fdd7e38ea058f5bfc92e133f9bf20c3
AAKASH <101560018+Aakash9513@users.noreply.github.com>
2023-09-17 13:32:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # import mysql.connector
-
+import sys
 # mydb=mysql.connector.connect(
 #     host="localhost",
 #     user="root",
@@ -8,6 +8,10 @@
 # )
 
 # mycursor=mydb.cursor()
+
+#There are two tables for this project
+#Tabel1 - "users" have the user's details
+#Table2 - "admin_login" have the admin details
 
 def transaction():
 
@@ -20,6 +24,7 @@ def transaction():
         user.main_function()
     else:
         print("thank you for your time")
+        sys.exit()
 
 def main():
     print("Welcome SMA Automated teller machine")
@@ -31,12 +36,12 @@ def main():
         transaction()
     elif user==2:
         import admin
-        admin.main_function()
+        admin.admin_login()
     else:
         print("pls type 1 or 2 ")
 main()
-
-# *********This function is just to add data in the data base***********
+            
+# *********This function is just to add data in the database in user table***********
 
 
 # def insert_data():
@@ -48,6 +53,22 @@ main()
 #     balance=input("enter your appoinment balance")
 #     pin_admin=input("enter your admin pin")  # admin will keep the pin same for all the accounts
 #     values=(id,user_name,account_number,balance,pin,pin_admin)
+#     mycursor=mydb.cursor(sql,values)
+#     mycursor.execute(sql,values)
+#     mydb.commit()
+#     print("Hi your account has been added  sucesfully")
+
+# insert_data()
+
+# *********This function is just to add data in the database in the admin_login table***********
+
+                # *************************
+
+# def insert_data():
+#     sql="insert into admin_login (admin_id,pin_number) values (%s,%s,)"
+#     id=input("enter your id")
+#     pin_number=input("enter your admin pin")  
+#     values=(id,pin_admin)
 #     mycursor=mydb.cursor(sql,values)
 #     mycursor.execute(sql,values)
 #     mydb.commit()
